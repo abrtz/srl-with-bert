ddae3e4ee4254164b16abb0fba3bd4dd5bf4a68d
Farnaz-BNF <160543501+Farnaz-BNF@users.noreply.github.com>
2024-03-03 05:01:07 +0330

    Create script_for_predicate_form.py

---
+++ script_for_predicate_form.py
@@ -0,0 +1,85 @@
+import pandas as pd
+
+
+def read_data_as_sentence(file_path, output_path):
+    """
+    Parses the CoNNL-U Plus file and returns a dataframe of sentences.
+    Extract features from the data and return a datarame.
+
+    Returns a dataframe, where each row represents one sentence with its all words and all features of words (each columns is a list with lengh of number of words in sentence).
+
+    file_path (str): The file path to the data to be preprocessed.
+    output_path (str): The file path to the save processed dataframe.
+    """
+
+    sentences = []
+    sentence = []  # Initialize an empty list for the current sentence
+    with open(file_path, 'r', encoding="utf8") as file:
+        for line in file:
+            line = line.strip().split('\t')
+            # If the line starts with '#', it's a comment, ignore it
+            if line[0].startswith('#'):
+                continue
+            elif line[0].strip() != '':
+
+                # Create a token if its ID does not contain a period
+                if '.' not in line[0] and len(line) > 10:
+                    token = {
+                        'form': line[1],
+                        'predicate': line[10],
+                        'argument': line[11:]  # Store all remaining elements as arguments.
+                    }
+                    # Append the token to the sentence.
+                    sentence.append(token)
+
+            # A new line indicates the end of a sentence.
+            elif line[0].strip() == '':
+                # Append the completed sentence to the sentences list.
+                sentences.append(sentence)
+                # Reset sentence for the next sentence.
+                sentence = []
+
+    # Iterate over all sentences. Create copies of sentences for each predicate.
+    expanded_sentences = []
+    for sentence in sentences:
+        # Find all predicates in the sentence.
+        predicates = [token['predicate'] for token in sentence if token['predicate'] != '_']
+
+        # for every predicate, create a copy of the sentence.
+        for index, predicate in enumerate(predicates):
+            sentence_copy = [token.copy() for token in sentence]
+            predicate_form = [token['form'] for token in sentence_copy if token['predicate'] == predicate]
+            for token in sentence_copy:
+
+                token['predicate'] = predicate_form[0]
+                
+                # Keep only the relevant argument for this predicate. Overwrite 'V' with '_'.
+                token['argument'] = token['argument'][index] if token['argument'][index] != 'V' else '_'
+            expanded_sentences.append(sentence_copy)
+
+    # Create a list to store each sentence.
+    final_list = []
+    # Iterate over all sentences after copy sentences for each predicate.
+    for sentence in expanded_sentences:
+        # Create empty lists for eah feature of data.
+        form_list =[]
+        argument_list =[]
+        # For each word in sentence append features in their list
+        # each list has all its feature for all words in sentence
+        for word in sentence:
+            form_list.append(word['form'])
+            argument_list.append(word['argument'])
+        argument_list.append(None)
+        argument_list.append('_')
+        form_list.append('[SEP]')
+        form_list.append(str(sentence[0]['predicate']).split('.')[0])
+        # After all words in a sentence processed, append all list of sentence to final list as a dict.
+        final_list.append({
+                        'input_form': form_list,
+                        'argument': argument_list})
+    # Convert list to pandas dataframe
+    df = pd.DataFrame(final_list)
+    # Save Dataframe to output_path
+    df.to_csv(output_path)
+    # return Dataframe
+    return df
